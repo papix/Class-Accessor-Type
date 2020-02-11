1065eaa05942658c60cb7cf263db8f99e3d32070
papix <mail@papix.net>
2020-02-11 16:46:54 +0900

    implement mk_* methods

--- lib/Class/Accessor/Typed.pm
+++ lib/Class/Accessor/Typed.pm
@@ -12,6 +12,7 @@ our $VERSION = "0.02";
 *_get_does_type_constraint = \&Mouse::Util::TypeConstraints::find_or_create_does_type_constraint;
 
 our $VERBOSE = 1;
+our %RULES;
 
 my %key_ctor = (
     rw      => \&_mk_accessors,
@@ -25,41 +26,99 @@ sub import {
     my ($class, %args) = @_;
     my $pkg = caller(0);
 
-    my %rules;
-    for my $key (sort keys %key_ctor) {
-        if (defined $args{$key}) {
-            croak("value of the '$key' parameter should be an hashref") unless ref($args{$key}) eq 'HASH';
+    for my $ctor (sort keys %key_ctor) {
+        if (defined $args{$ctor}) {
+            croak("value of the '$ctor' parameter should be an hashref") unless ref($args{$ctor}) eq 'HASH';
 
-            for my $n (sort keys %{$args{$key}}) {
-                my $rule = ref($args{$key}->{$n}) eq 'HASH'
-                    ? $args{$key}->{$n} : { isa => $args{$key}->{$n} };
-
-                $rule->{type} = do {
-                    if (defined $rule->{isa}) {
-                        _get_isa_type_constraint($rule->{isa});
-                    } elsif (defined $rule->{does}) {
-                        _get_does_type_constraint($rule->{does});
-                    }
-                };
-                $rule->{lazy} = ($key eq 'rw_lazy' or $key eq 'ro_lazy') ? 1 : 0;
-
-                $args{$key}->{$n} = $rule;
-                $rules{$n}        = $rule;
-            }
-            $key_ctor{$key}->($pkg, %{$args{$key}});
+            _register_rules($pkg, $ctor, %{$args{$ctor}});
+            $key_ctor{$ctor}->($pkg, keys %{$args{$ctor}});
         }
     }
     return 1 if exists $args{new} && !$args{new};
 
-    _mk_new($pkg, %rules);
+    _mk_new($pkg);
     return 1;
+}
+
+sub _register_rules {
+    my $pkg = shift;
+    my $ctor = shift;
+    my %rules = @_;
+
+    for my $n (sort keys %rules) {
+        my $rule = $rules{$n};
+        _register_rule($pkg, $ctor, $n, $rule);
+    }
+}
+
+sub _register_rule {
+    my $pkg = shift;
+    my $ctor = shift;
+    my $n = shift;
+    my $rule = ref $_[0] eq 'HASH' ? $_[0] : { isa => $_[0] };
+
+    $rule->{type} = do {
+        if (defined $rule->{isa}) {
+            _get_isa_type_constraint($rule->{isa});
+        } elsif (defined $rule->{does}) {
+            _get_does_type_constraint($rule->{does});
+        }
+    };
+    $rule->{lazy} = ($ctor eq 'rw_lazy' or $ctor eq 'ro_lazy') ? 1 : 0;
+
+    $RULES{$pkg}->{$n} = $rule;
+}
+
+sub mk_new {
+    my $pkg = caller(0);
+    _mk_new($pkg);
+}
+
+sub mk_accessors {
+    (undef, my %args) = @_;
+    my $pkg = caller(0);
+
+    _register_rules($pkg, 'rw', %args);
+    _mk_accessors($pkg, keys %args);
+}
+
+sub mk_ro_accessors {
+    (undef, my %args) = @_;
+    my $pkg = caller(0);
+
+    _register_rules($pkg, 'ro', %args);
+    _mk_ro_accessors($pkg, keys %args);
+}
+
+sub mk_wo_accessors {
+    (undef, my %args) = @_;
+    my $pkg = caller(0);
+
+    _register_rules($pkg, 'wo', %args);
+    _mk_wo_accessors($pkg, keys %args);
+}
+
+sub mk_lazy_accessors {
+    (undef, my %args) = @_;
+    my $pkg = caller(0);
+
+    _register_rules($pkg, 'rw_lazy', %args);
+    _mk_lazy_accessors($pkg, keys %args);
+}
+
+sub mk_ro_lazy_accessors {
+    (undef, my %args) = @_;
+    my $pkg = caller(0);
+
+    _register_rules($pkg, 'ro_lazy', %args);
+    _mk_ro_lazy_accessors($pkg, keys %args);
 }
 
 sub _mk_new {
     my $pkg = shift;
     no strict 'refs';
 
-    *{$pkg . '::new'} = __m_new($pkg, @_);
+    *{$pkg . '::new'} = __m_new($pkg);
 }
 
 sub _mk_accessors {
@@ -68,8 +127,7 @@ sub _mk_accessors {
 
     while (@_) {
         my $n = shift;
-        my $rule = shift;
-        *{$pkg . '::' . $n} = __m($n, $rule->{type});
+        *{$pkg . '::' . $n} = __m($pkg, $n);
     }
 }
 
@@ -79,7 +137,6 @@ sub _mk_ro_accessors {
 
     while (@_) {
         my $n = shift;
-        my $rule = shift;
         *{$pkg . '::' . $n} = __m_ro($pkg, $n);
     }
 }
@@ -90,8 +147,7 @@ sub _mk_wo_accessors {
 
     while (@_) {
         my $n = shift;
-        my $rule = shift;
-        *{$pkg . '::' . $n} = __m_wo($pkg, $n, $rule->{type});
+        *{$pkg . '::' . $n} = __m_wo($pkg, $n);
     }
 }
 
@@ -101,9 +157,8 @@ sub _mk_lazy_accessors {
 
     while (@_) {
         my $n = shift;
-        my $rule = shift;
-        my $builder = $rule->{builder} || "_build_$n";
-        *{$pkg . '::' . $n} = __m_lazy($n, $rule->{type}, $builder);
+        my $builder = $RULES{$pkg}->{$n}->{builder} || "_build_$n";
+        *{$pkg . '::' . $n} = __m_lazy($pkg, $n, $builder);
     }
 }
 
@@ -113,21 +168,20 @@ sub _mk_ro_lazy_accessors {
 
     while (@_) {
         my $n = shift;
-        my $rule = shift;
-        my $builder = $rule->{builder} || "_build_$n";
-        *{$pkg . '::' . $n} = __m_ro_lazy($pkg, $n, $rule->{type}, $builder);
+        my $builder = $RULES{$pkg}->{$n}->{builder} || "_build_$n";
+        *{$pkg . '::' . $n} = __m_ro_lazy($pkg, $n, $builder);
     }
 }
 
 sub __m_new {
     my $pkg = shift;
-    my %rules = @_;
     no strict 'refs';
     return sub {
         my $klass = shift;
         my %args = (@_ == 1 && ref($_[0]) eq 'HASH' ? %{$_[0]} : @_);
         my %params;
 
+        my %rules = %{ $RULES{$pkg} };
         for my $n (sort keys %rules) {
             if (! exists $args{$n}) {
                 next if $rules{$n}->{lazy};
@@ -149,11 +203,11 @@ sub __m_new {
 }
 
 sub __m {
-    my ($n, $type) = @_;
+    my ($pkg, $n) = @_;
 
     sub {
         return $_[0]->{$n} if @_ == 1;
-        return $_[0]->{$n} = _check($n, $type, $_[1]) if @_ == 2;
+        return $_[0]->{$n} = _check($n, $RULES{$pkg}->{$n}->{type}, $_[1]) if @_ == 2;
     };
 }
 
@@ -168,35 +222,35 @@ sub __m_ro {
 }
 
 sub __m_wo {
-    my ($pkg, $n, $type) = @_;
+    my ($pkg, $n) = @_;
 
     sub {
-        return $_[0]->{$n} = _check($n, $type, $_[1]) if @_ == 2;
+        return $_[0]->{$n} = _check($n, $RULES{$pkg}->{$n}->{type}, $_[1]) if @_ == 2;
         my $caller = caller(0);
         error("'$caller' cannot alter the value of '$n' on objects of class '$pkg'");
     };
 }
 
 sub __m_lazy {
-    my ($n, $type, $builder) = @_;
+    my ($pkg, $n, $builder) = @_;
 
     sub {
         if (@_ == 1) {
             return $_[0]->{$n} if exists $_[0]->{$n};
-            return $_[0]->{$n} = _check($n, $type, $_[0]->$builder);
+            return $_[0]->{$n} = _check($n, $RULES{$pkg}->{$n}->{type}, $_[0]->$builder);
         } elsif (@_ == 2) {
-            return $_[0]->{$n} = _check($n, $type, $_[1]);
+            return $_[0]->{$n} = _check($n, $RULES{$pkg}->{$n}->{type}, $_[1]);
         }
     };
 }
 
 sub __m_ro_lazy {
-    my ($pkg, $n, $type, $builder) = @_;
+    my ($pkg, $n, $builder) = @_;
 
     sub {
         if (@_ == 1) {
             return $_[0]->{$n} if exists $_[0]->{$n};
-            return $_[0]->{$n} = _check($n, $type, $_[0]->$builder);
+            return $_[0]->{$n} = _check($n, $RULES{$pkg}->{$n}->{type}, $_[0]->$builder);
         }
         my $caller = caller(0);
         error("'$caller' cannot alter the value of '$n' on objects of class '$pkg'");
@@ -270,7 +324,7 @@ Class::Accessor::Typed is variant of C<Class::Accessor::Lite>. It supports argum
 =head1 THE USE STATEMENT
 
 The use statement of the module takes a single hash.
-An arguments specifies the read/write type (rw, ro, wo) and setting of properties.
+An arguments specifies the read/write type (rw, ro, wo, rw_lazy and ro_lazy) and setting of properties.
 Setting of property is defined by hash reference that specifies property name as key and property rule as value.
 
     use Class::Accessor::Typed (
--- t/01_base.t
+++ t/01_base.t
@@ -20,10 +20,26 @@ use Test::Exception;
             wo => 'Int',
         },
     );
-}
-{
+
     package
         M;
+    
+    use Class::Accessor::Typed;
+
+    Class::Accessor::Typed->mk_accessors(
+        rw1 => { isa => 'Str', default => 'default value' },
+        rw2 => 'Int',
+    );
+    Class::Accessor::Typed->mk_ro_accessors(
+        ro1 => 'Str',
+        ro2 => 'Int',
+    );
+    Class::Accessor::Typed->mk_wo_accessors(
+        wo => 'Int',
+    );
+
+    package
+        N;
 
     use Class::Accessor::Typed (
         rw => {
@@ -34,117 +50,129 @@ use Test::Exception;
 }
 
 subtest 'new' => sub {
-    my $obj = L->new(
-        rw1 => 'RW1',
-        rw2 => 321,
-        ro1 => 'RO1',
-        ro2 => 123,
-        wo  => 222,
-    );
-    isa_ok $obj, 'L';
-
-    subtest 'validation error' => sub {
-        throws_ok {
-            L->new(
-                rw1 => 'RW1',
-                rw2 => 'RW2',
-                ro1 => 'RO1',
-                ro2 => 123,
-                wo  => 222,
-            );
-        } qr/'rw2': Validation failed for 'Int' with value RW2/;
-    };
-
-    subtest 'missing mandatory parameter' => sub {
-        throws_ok {
-            L->new(
+    for my $pkg (qw/L M/) {
+        subtest $pkg => sub {
+            my $obj = $pkg->new(
                 rw1 => 'RW1',
                 rw2 => 321,
                 ro1 => 'RO1',
                 ro2 => 123,
+                wo  => 222,
             );
-        } qr/missing mandatory parameter named '\$wo'/;
-    };
+            isa_ok $obj, $pkg;
 
-    subtest 'default option' => sub {
-        my $obj = L->new(
-            rw2 => 321,
-            ro1 => 'RO1',
-            ro2 => 123,
-            wo  => 222,
-        );
+            subtest 'validation error' => sub {
+                throws_ok {
+                    $pkg->new(
+                        rw1 => 'RW1',
+                        rw2 => 'RW2',
+                        ro1 => 'RO1',
+                        ro2 => 123,
+                        wo  => 222,
+                    );
+                } qr/'rw2': Validation failed for 'Int' with value RW2/;
+            };
 
-        is $obj->rw1, 'default value';
-    };
+            subtest 'missing mandatory parameter' => sub {
+                throws_ok {
+                    $pkg->new(
+                        rw1 => 'RW1',
+                        rw2 => 321,
+                        ro1 => 'RO1',
+                        ro2 => 123,
+                    );
+                } qr/missing mandatory parameter named '\$wo'/;
+            };
 
-    subtest 'unknown arguments' => sub {
-        my $warn = '';
-        local $SIG{__WARN__} = sub {
-            $warn .= "@_";
+            subtest 'default option' => sub {
+                my $obj = $pkg->new(
+                    rw2 => 321,
+                    ro1 => 'RO1',
+                    ro2 => 123,
+                    wo  => 222,
+                );
+
+                is $obj->rw1, 'default value';
+            };
+
+            subtest 'unknown arguments' => sub {
+                my $warn = '';
+                local $SIG{__WARN__} = sub {
+                    $warn .= "@_";
+                };
+
+                my $obj = $pkg->new(
+                    rw1     => 'RW1',
+                    rw2     => 321,
+                    ro1     => 'RO1',
+                    ro2     => 123,
+                    wo      => 222,
+                    unknown => 'unknown',
+                );
+
+                like $warn, qr/unknown arguments: unknown/;
+                isa_ok $obj, $pkg;
+                ok ! exists $obj->{unknown};
+            };
         };
-
-        my $obj = L->new(
-            rw1     => 'RW1',
-            rw2     => 321,
-            ro1     => 'RO1',
-            ro2     => 123,
-            wo      => 222,
-            unknown => 'unknown',
-        );
-
-        like $warn, qr/unknown arguments: unknown/;
-        isa_ok $obj, 'L';
-        ok ! exists $obj->{unknown};
-    };
+    }
 
     subtest 'disable new option' => sub {
         throws_ok {
-            M->new(rw => 'RW');
-        } qr/Can't locate object method "new" via package "M"/;
+            N->new(rw => 'RW');
+        } qr/Can't locate object method "new" via package "N"/;
     };
 };
 
 subtest 'getter' => sub {
-    my $obj = L->new(
-        rw1 => 'RW1',
-        rw2 => 321,
-        ro1 => 'RO1',
-        ro2 => 123,
-        wo  => 222,
-    );
-
-    is $obj->rw1, 'RW1';
-    is $obj->rw2, 321;
-    is $obj->ro1, 'RO1';
-    is $obj->ro2, 123;
-
-    throws_ok {
-        $obj->wo;
-    } qr/cannot alter the value of 'wo' on objects of class 'L'/;
+    for my $pkg (qw/L M/) {
+        subtest $pkg => sub {
+            my $obj = $pkg->new(
+                rw1 => 'RW1',
+                rw2 => 321,
+                ro1 => 'RO1',
+                ro2 => 123,
+                wo  => 222,
+            );
+        
+            is $obj->rw1, 'RW1';
+            is $obj->rw2, 321;
+            is $obj->ro1, 'RO1';
+            is $obj->ro2, 123;
+        
+            throws_ok {
+                $obj->wo;
+            } qr/cannot alter the value of 'wo' on objects of class '$pkg'/;
+        };
+    }
 };
 
 subtest 'setter' => sub {
-    my $obj = L->new(
-        rw1 => 'RW1',
-        rw2 => 321,
-        ro1 => 'RO1',
-        ro2 => 123,
-        wo  => 222,
-    );
-
-    $obj->rw1('sample');
-    is $obj->rw1, 'sample';
-
-    throws_ok {
-        $obj->rw2('sample');
-    } qr/'rw2': Validation failed for 'Int' with value sample/;
-
-    throws_ok {
-        $obj->ro1('sample');
-    } qr/cannot access the value of 'ro1' on objects of class 'L'/;
-
-    $obj->wo(333);
-    is $obj->{wo}, 333;
+    for my $pkg (qw/L M/) {
+        subtest $pkg => sub {
+            my $obj = $pkg->new(
+                rw1 => 'RW1',
+                rw2 => 321,
+                ro1 => 'RO1',
+                ro2 => 123,
+                wo  => 222,
+            );
+        
+            $obj->rw1('sample');
+            is $obj->rw1, 'sample';
+        
+            throws_ok {
+                $obj->rw2('sample');
+            } qr/'rw2': Validation failed for 'Int' with value sample/;
+        
+            throws_ok {
+                $obj->ro1('sample');
+            } qr/cannot access the value of 'ro1' on objects of class '$pkg'/;
+        
+            $obj->wo(333);
+            is $obj->{wo}, 333;
+        };
+    }
 };
 
 done_testing;
